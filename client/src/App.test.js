266c1328baa189db5ec4619373ff24fbb32bd416
Alex Butler <alexbutler@Don-Butlero.local>
2019-09-19 18:11:02 +0100

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  // obviously, don't actuall do this in a real project
+  // just forcing tests to pass
+});
