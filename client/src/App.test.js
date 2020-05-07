fdd457e090b212247f402f599d8f48daaedfd023
Alex Butler <alexbutler@Don-Butlero.local>
2020-05-07 09:41:48 +0100

    Initial commit to Kubernetes

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,7 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  // obviously, don't actuall do this in a real project
-  // just forcing tests to pass
-});
+it('renders without crashing', () => {});
