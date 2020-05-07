6977ce1f03c4a0828c2e64aa85da91e115273b39
Alex Butler <alexbutler@Don-Butlero.local>
2020-05-07 16:07:59 +0100

    Typo in deploy fixed

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
+echo Building client...
 docker build -t abutler93/multi-client:latest -t abutler93/multi-client:$SHA -f ./client/Dockerfile ./client
+echo Building server...
 docker build -t abutler93/multi-server:latest -t abutler93/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t abutler93/multi-worker:latest -t abutler93/multi-worker:$SHA-f ./worker/Dockerfile ./worker
+echo Building worker...
+docker build -t abutler93/multi-worker:latest -t abutler93/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 
 docker push abutler93/multi-client:latest
